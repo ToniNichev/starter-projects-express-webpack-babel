14bf96a6c46c49a4bff018a9025c21335775e246
ToniNichev <toni.nichev@gmail.com>
2021-11-15 13:28:19 -0500

    #stable

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # How to install
-    1. Navigate to ./server.js
-    2. Locate LAUNCH_DARKLY_API_KEY and add the key there.
     3. yarn install;yarn start;
 
 # Vew page
--- server.js
+++ server.js
@@ -1,43 +1,13 @@
 const express = require("express");
-const path = require("path");
-const LaunchDarkly = require('launchdarkly-node-server-sdk');
-
-const LAUNCH_DARKLY_API_KEY='[ADD-LD-API-KEY-HERE]';
 
 
-const ldClient = LaunchDarkly.init(LAUNCH_DARKLY_API_KEY, {
-    proxyHost: '127.0.0.1',
-    proxyPort: 8888
-});     
 
-
-//const ldClient = LaunchDarkly.init(LAUNCH_DARKLY_API_KEY);
 
 const app = express();
 const port = process.env.PORT || "8000";
 
-
-const getFlags = async () => {
-    const cnbcServerUser = {
-        key: 'cnbc-phoenix-server',
-      };    
-
-
-    let flags = { "error": "yes"};
-    try {
-        if(!ldClient.initialized()) {
-            await ldClient.waitForInitialization();
-        }        
-        flags = (await ldClient.allFlagsState(cnbcServerUser)).toJSON();
-    } catch( error ) {
-        console.log("Error fetching LD params: ", error);
-    }
-    return flags;
-}
-
 app.get("/", async (req, res) => {
-    const flags = await getFlags();
-    res.status(200).send(flags);    
+    res.status(200).send("Hello world!");    
 });
 
 app.use(express.static('public'))
